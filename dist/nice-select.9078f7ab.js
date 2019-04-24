eee92dd1c2684a1501a83aac4a20ba9ce8531fed
Irvins <irvins.cassull@gmail.com>
2019-04-24 17:48:35 +0300

    добавлен ввод опций для количества жильцов

--- dist/nice-select.9078f7ab.js
+++ dist/nice-select.9078f7ab.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"C:/Users/Konica/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/css/datepicker.css":[function(require,module,exports) {
+},{"./bundle-url":"C:/Users/Konica/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/css/nice-select.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/Konica/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/datepicker.4021c40f.js.map
+//# sourceMappingURL=/nice-select.9078f7ab.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,22 @@
 import main from './main';
 import './datepicker';
+import './jquery.nice-select';
 
 main();
 
 $(document).ready(function() {
-  $('.datepicker-there').datepicker({
+  $('.datepicker-plugin').datepicker({
     range: true,
-    multipleDatesSeparator: " - ",
+    multipleDatesSeparator: "                       ",
     position: "bottom center", 
     clearButton: true
   })
+
+  $('.guest-setter-input').on('blur', function() {
+    $('.guest-setter').fadeOut('medium');
+  });
+  $('.guest-setter-input').on('focus', function() {
+      $(this).siblings('.guest-setter').show();
+  });
+  $('.guest-setter').hide();
 });
