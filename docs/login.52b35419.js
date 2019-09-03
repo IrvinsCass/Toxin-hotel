638ece88ccd5cd46fe1c0b069f7f457bbdb644c5
Irvins <irvins.cassull@gmail.com>
2019-09-03 15:20:53 +0300

    Добавлен дропдаун
    - добавлен дропдаун для гостей
    -поправлены некоторые стили

--- docs/login.52b35419.js
+++ docs/login.52b35419.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"../scss/main.scss":"D3vG"}],"QNLl":[function(require,module,exports) {
 "use strict";var e=u(require("./main"));function u(e){return e&&e.__esModule?e:{default:e}}(0,e.default)();
 },{"./main":"aR/y"}]},{},["QNLl"], null)
-//# sourceMappingURL=login.6068eec8.js.map
+//# sourceMappingURL=login.52b35419.js.map
